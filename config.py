ed0031803f1a8218a42d4ae9cacafbd7ed2ccefb
glenn6452 <dalidaglenno6452@gmail.com>
2019-07-12 02:34:59 +0800

    no message

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ c.Exporter.preprocessors = ['nbconvert.preprocessors.ExecutePreprocessor']
 c.NbConvertApp.postprocessor_class = 'nb2mail.SendMailPostProcessor'
 c.SendMailPostProcessor.recipient = 'glenn.dalida@oriente.com.ph'
 c.SendMailPostProcessor.smtp_user = 'glenn.dalida@oriente.com.ph'
-c.SendMailPostProcessor.smtp_pass = 'Zu=8uthe'
+c.SendMailPostProcessor.smtp_pass = 'Zu@8uthe'
 c.SendMailPostProcessor.smtp_addr = 'smtp.office365.com'
 c.SendMailPostProcessor.smtp_port = 587
 c.MailExporter.anchor_link_text = '' # disable pilcrow, requires nbconvert >= 5.2
